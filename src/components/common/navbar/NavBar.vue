cc8c81e25d61af60f7d471a2b3145705ed7c1d0c
cloud_nzk <526416486@qq.com>
2022-06-29 23:25:10 +0800

    NavBar封装完成

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- 跟封装 Tabbar 的思路类似，留左中右 3 个插槽  -->
+  <div class="nav-bar">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  destroyed() {},
+  methods: {},
+};
+</script>
+<style scoped>
+  .nav-bar{
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        box-shadow: 0 1px 1px rgba(100,100,100,.1);
+    }
+
+    .left,.right{
+        width: 60px;
+        /* background-color: red; */
+    }
+
+    .center{
+        flex: 1;
+        /* background-color: blue; */
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    首页
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
   export default {
     name:'',
     mixins: [],
-    components: {},
+    components: {
+      NavBar,
+    },
     props:{},
     data () {
       return {
@@ -23,5 +28,8 @@
   }
 </script>
 <style scoped>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
