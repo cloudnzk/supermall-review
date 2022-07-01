a3376e6ed382530c9464c7d82b1d58458db8b1fe
cloud_nzk <526416486@qq.com>
2022-07-01 16:21:29 +0800

    首页轮播图完成：使用现有的vue-awesome-swiper组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,20 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from 'views/home/childComps/HomeSwiper'
+
   import {getHomeMultidata} from 'network/home'
   export default {
     name:'',
     mixins: [],
     components: {
       NavBar,
+      HomeSwiper
     },
     props:{},
     data () {
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="slide">
+    <swiper
+      class="swiper"
+      :options="swiperOption"
+    >
+      <swiper-slide v-for="item in banners" :key="item.link">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-slide>
+      <div class="swiper-pagination-white" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+  import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+  import 'swiper/css/swiper.css';
+
+  export default {
+    name:'',
+    mixins: [],
+    components: {
+      Swiper,
+      SwiperSlide
+    },
+    props: {
+      banners:{
+        type: Array,
+        // 设置默认值时要写成一个函数
+        default(){
+          return []
+        } ,
+      }
+    },
+    data () {
+      return {
+        swiperOption: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false
+          },
+          // effect: 'fade',
+          pagination: {
+            el: '.swiper-pagination-white',
+            dynamicBullets: true,
+          }
+          // pagination: {
+          //   el: '.swiper-pagination',
+          //   clickable: true
+          // }
+        }
+      };
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {},
+    destroyed() {},
+    methods: {
+      //  onSlideChange(){
+      //     console.log('111');
+      //  }
+    },
+  }
+</script>
+<style scoped>
+  .slide{
+    width: 100%;
+  }
+
+  .slide img{
+    width: 100%;
+  }
+</style>
